7c025c25fd2dc1de6500d45aaaafcaba75a7c6df
Theo Ward <theoward9@gmail.com>
2022-03-26 16:46:43 -0500

    added resume, images to modals, info to modals, links to modals, background color

---
+++ script.js
@@ -0,0 +1,20 @@
+// const modal1Deploy = getElementById('modal1Deploy');
+// const modal1Git = getElementById('modal1Git');
+
+// const modal2Deploy = getElementById('modal2Deploy');
+// const modal2Git = getElementById('modal2Git');
+
+// const modal3Deploy = getElementById('modal3Deploy');
+// const modal3Git = getElementById('modal3Git');
+
+// const modal4Deploy = getElementById('modal4Deploy');
+// const modal4Git = getElementById('modal4Git');
+
+// const modal5Deploy = getElementById('modal5Deploy');
+// const modal5Git = getElementById('modal5Git');
+
+// modal1Deploy.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     event.stopImediatePropogation();
+
+// });
